7a22682ea743150d10ac26087939fa39dbfef0b2
sundar410 <43734758+sundar410@users.noreply.github.com>
2022-03-02 15:40:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">Home App - version-2!!</h1> \n');
+  res.send('<h1 style="color:green;">Home App - version-3!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
